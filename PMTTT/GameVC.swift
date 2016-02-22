fa9cf8fe0cf3f5fe430a6faddf47021e817c7543
Pavle Mijatovic <paul.mijatovic@gmail.com>
2016-02-22 10:43:40 +0100

    Refactor

--- PMTTT/GameVC.swift
+++ PMTTT/GameVC.swift
@@ -30,44 +30,33 @@ class GameVC: UIViewController
     @IBOutlet weak var playerInfo: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
     
-    
     var didPlayerOneWin = false
     var isItATie = false
     
-    var goNumber = 1
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    var winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-    var winner = 0
+    var goNum = 1
+    var game = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var gameWinCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    var winn = 0
     
     let playerX = "Player X"
     let playerO = "Player O"
     let tieMessage = "it was a tie"
     let noWinnerMessage = "No winner!"
     let startingMessage = "X starts first"
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillAppear:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillDisappear:", name: UIKeyboardWillHideNotification, object: nil)
+        addGestureRecognizers()
+        addObservers()
     }
-    
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.hidden = true
-        
         generalInfo.hidden = false
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        generalInfo.center = CGPointMake(generalInfo.center.x - 400, generalInfo.center.y)
-        //        playAgainButton.alpha = 0
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -76,15 +65,32 @@ class GameVC: UIViewController
     }
     
     
-    func keyboardWillAppear(notification: NSNotification){
-        
+    // MARK: - Public
+    func keyboardWillAppear(notification: NSNotification) {
         enableBoardFields(false)
     }
     
-    func keyboardWillDisappear(notification: NSNotification){
-        
+    func keyboardWillDisappear(notification: NSNotification) {
         enableBoardFields(true)
-        
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    
+    
+    
+    // MARK: - Private
+    private func addGestureRecognizers() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    private func addObservers() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillAppear:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"keyboardWillDisappear:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     private func enableBoardFields(shouldEnable: Bool) {
@@ -93,110 +99,7 @@ class GameVC: UIViewController
         b31.enabled = shouldEnable; b32.enabled = shouldEnable; b33.enabled = shouldEnable;
     }
     
-    //MARK: - private
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
-    }
-    
-    @IBAction func playAction(sender: UIButton) {
-        
-        generalInfo.text = ""
-        
-        if gameState[sender.tag] == 0 && winner == 0
-        {
-            var image = UIImage()
-            
-            if goNumber % 2 == 0
-            {
-                
-                
-                image = UIImage(named: "o")!
-                gameState[sender.tag] = 1
-                
-                let p1 = player1.text! == "" ? playerX : player1.text!
-                
-                playerInfo.text = "It's \(p1)'s Turn"
-            }
-            else
-            {
-                
-                let p2 = player1.text! == "" ? playerO : player2.text!
-                
-                
-                image = UIImage(named: "x")!
-                gameState[sender.tag] = 2
-                playerInfo.text = "It's \(p2)'s Turn"
-                
-            }
-            
-            for combination in winningCombinations
-            {
-                if gameState[combination[0]] == gameState[combination[1]] &&
-                    gameState[combination[0]] == gameState[combination[2]] &&
-                    gameState[combination[0]] != 0
-                {
-                    winner = gameState[combination[0]]
-                }
-            }
-            
-            if winner != 0
-            {
-                
-                if winner == 1
-                {
-                    let p2 = player1.text! == "" ? playerO : player2.text!
-                    
-                    generalInfo.text = "\(p2) has won!"
-                    self.isItATie = false
-                    
-                    playerInfo.text = ""
-                    
-                    self.didPlayerOneWin = false
-                    
-                    inputStatData()
-                    
-                }
-                else
-                {
-                    let p1 = player1.text! == "" ? playerX : player1.text!
-                    
-                    generalInfo.text = "\(p1) has won!"
-                    self.didPlayerOneWin = true
-                    self.isItATie = false
-                    playerInfo.text = ""
-                    
-                    inputStatData()
-                }
-                
-                self.generalInfo.hidden = false
-                self.playAgainButton.hidden = false
-                
-            }
-            
-            goNumber++
-            sender.setImage(image, forState: UIControlState.Normal)
-            
-            if goNumber == 10 && winner == 0
-            {
-                generalInfo.text = noWinnerMessage
-                
-                self.isItATie = true
-                self.generalInfo.hidden = false
-                self.generalInfo.center = CGPointMake(self.generalInfo.center.x + 400, self.generalInfo.center.y)
-                self.playAgainButton.hidden = false
-                
-                self.playerInfo.text = ""
-                
-                isItATie = true
-                didPlayerOneWin = false
-                inputStatData()
-                
-            }
-        }
-    }
-    
-    func inputStatData() {
+    private func saveStatData() {
         let timestamp = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .MediumStyle, timeStyle: .ShortStyle)
         
         var finalStatMessage = ""
@@ -213,14 +116,17 @@ class GameVC: UIViewController
                 winnerName = p1
             } else {
                 let p2 = player1.text! == "" ? playerO : player2.text!
-
+                
                 winnerName = p2
             }
             
             finalStatMessage = "on the \(timestamp), \(winnerName) won"
         }
         
-        
+        saveStatObjectToDB(withTimeStamp: timestamp, andFinalMessage: finalStatMessage)
+    }
+    
+    private func saveStatObjectToDB(withTimeStamp timestamp: String, andFinalMessage finalStatMessage:String ) {
         let statsObject = StatsRealm()
         
         statsObject.player1 = player1.text!
@@ -228,17 +134,83 @@ class GameVC: UIViewController
         statsObject.date = timestamp
         statsObject.finalMessage = finalStatMessage
         
-        
         let dbHandler = DBHandler()
         dbHandler.addStatsToDB(statsObject)
+    }
 
+    
+    // MARK: - Actions
+    @IBAction func playAction(sender: UIButton) {
+        
+        generalInfo.text = ""
+        
+        if game[sender.tag] == 0 && winn == 0 {
+            var image = UIImage()
+            
+            if goNum % 2 == 0 {
+                image = UIImage(named: "o")!
+                game[sender.tag] = 1
+                let p1 = player1.text! == "" ? playerX : player1.text!
+                playerInfo.text = "It's \(p1)'s Turn"
+            } else {
+                let p2 = player1.text! == "" ? playerO : player2.text!
+                image = UIImage(named: "x")!
+                game[sender.tag] = 2
+                playerInfo.text = "It's \(p2)'s Turn"
+            }
+            
+            for combination in gameWinCombo {
+                if game[combination[0]] == game[combination[1]] && game[combination[0]] == game[combination[2]] && game[combination[0]] != 0 {
+                    winn = game[combination[0]]
+                }
+            }
+            
+            if winn != 0 {
+                if winn == 1 {
+                    let p2 = player1.text! == "" ? playerO : player2.text!
+                    generalInfo.text = "\(p2) has won!"
+                    self.isItATie = false
+                    playerInfo.text = ""
+                    self.didPlayerOneWin = false
+                    saveStatData()
+                } else {
+                    let p1 = player1.text! == "" ? playerX : player1.text!
+                    generalInfo.text = "\(p1) has won!"
+                    self.didPlayerOneWin = true
+                    self.isItATie = false
+                    playerInfo.text = ""
+                    
+                    saveStatData()
+                }
+                
+                self.generalInfo.hidden = false
+                self.playAgainButton.hidden = false
+            }
+            
+            goNum++
+            sender.setImage(image, forState: UIControlState.Normal)
+            
+            if goNum == 10 && winn == 0 {
+                generalInfo.text = noWinnerMessage
+                
+                self.isItATie = true
+                self.generalInfo.hidden = false
+//                self.generalInfo.center = CGPointMake(self.generalInfo.center.x + 400, self.generalInfo.center.y)
+                self.playAgainButton.hidden = false
+                
+                self.playerInfo.text = ""
+                
+                isItATie = true
+                didPlayerOneWin = false
+                saveStatData()
+            }
+        }
     }
     
     @IBAction func playAgainButtonPressed(sender: UIButton) {
-        
-        goNumber = 1
-        winner = 0
-        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        goNum = 1
+        winn = 0
+        game = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         generalInfo.text = startingMessage
         
         self.playAgainButton.hidden = true
